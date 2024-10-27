c25e5b790a29bdfa74f8f6ef1fcac95dfc78cfdc
Huugo <huugoalexanndre@gmail.com>
2024-10-26 21:19:29 -0300

    Métodos ordenados

--- Conceito/ListaEncadeada/main.py
+++ Conceito/ListaEncadeada/main.py
@@ -15,7 +15,7 @@ class Noh:
     def setNext(self, novo_proximo):
         self.proximo = novo_proximo
 
-class ListaNaoOrdenada:
+class Lista:
     def __init__(self):
         self.head = None
 
@@ -26,6 +26,26 @@ class ListaNaoOrdenada:
         temp = Noh(item)
         temp.setNext(self.head)
         self.head = temp
+
+    def add_ordenada(self, item):
+        atual = self.head
+        anterior = None
+        parar = False
+
+        while atual != None and not parar:
+            if atual.getData() > item:
+                parar = True
+            else:
+                anterior = atual
+                atual = atual.getNext()
+        
+        temp = Noh(item)
+        if anterior == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(atual)
+            anterior.setNext(temp)
 
     def size(self):
         atual = self.head
@@ -44,6 +64,20 @@ class ListaNaoOrdenada:
             else:
                 atual = atual.getNext()
         return encontrou
+
+    def search_ordenada(self, item):
+        atual = self.head
+        encontrou = False
+        parar = False
+
+        while atual != None and not encontrou and not parar:
+            if atual.getData() == item:
+                encontrou = True
+            else:
+                if atual.getData() > item:
+                    parar = True
+                else:
+                    atual = atual.getNext()
 
     def remove(self, item):
         atual = self.head
@@ -68,14 +102,30 @@ class ListaNaoOrdenada:
             elementos.append(str(atual.getData()))
             atual = atual.getNext()
         return " -> ".join(elementos)
-    
-teste = ListaNaoOrdenada()
+
+# Não ordenada     
+teste = Lista()
+teste.add(3)
 teste.add(1)
 teste.add(2)
-teste.add(3)
+teste.add(4)
 print(teste)
 print("Tamanho: ", teste.size())
 print("Vazio? " ,teste.is_empty())
 teste.remove(2)
 print(teste)
 print("Tamanho: ", teste.size())
+
+print("#########################")
+# Ordenada
+teste2 = Lista()
+teste2.add_ordenada(3)
+teste2.add_ordenada(1)
+teste2.add_ordenada(2)
+teste2.add_ordenada(4)
+print(teste2)
+print("Tamanho: ", teste2.size())
+print("Vazio? " ,teste2.is_empty())
+teste2.remove(2)
+print(teste2)
+print("Tamanho: ", teste2.size())
