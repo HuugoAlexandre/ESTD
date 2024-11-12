6c7eb98bf098e3752c4cdce24a9ce8c41e202f91
HuugoAlexandre <huugoalexanndre@gmail.com>
2024-11-12 14:08:38 -0300

    Código principal

---
+++ Conceito/Hash/main.py
@@ -0,0 +1,55 @@
+class HashTable:
+    def __init__(self):
+        self._tamanho = 11
+        self._slots = [None] * self._tamanho
+        self._valores = [None] * self._tamanho
+
+    def hashfunction(self, chave, tamanho):
+        return chave % tamanho
+
+    def rehash(self, oldhash, tamanho):
+        return (oldhash + 1) % tamanho
+
+    def put(self, chave, valor):
+        valor_hash = self.hashfunction(chave, len(self._slots))
+        if self._slots[valor_hash] is None:
+            self._slots[valor_hash] = chave
+            self._valores[valor_hash] = valor
+        else:
+            if self._slots[valor_hash] == chave:
+                self._valores[valor_hash] = valor
+            else:
+                proximo_slot = self.rehash(valor_hash, len(self._slots))
+                while self._slots[proximo_slot] is not None and \
+                        self._slots[proximo_slot] != chave:
+                    proximo_slot = self.rehash(proximo_slot, len(self._slots))
+                if self._slots[proximo_slot] is None:
+                    self._slots[proximo_slot] = chave
+                    self._valores[proximo_slot] = valor
+                else:
+                    self._valores[proximo_slot] = valor
+
+    def get(self, chave):
+        slot_inicial = self.hashfunction(chave, len(self._slots))
+        valor = None
+        parar = False
+        encontrou = False
+        posicao = slot_inicial
+        while self._slots[posicao] is not None and not encontrou and not parar:
+            if self._slots[posicao] == chave:
+                encontrou = True
+                valor = self._valores[posicao]
+            else:
+                posicao = self.rehash(posicao, len(self._slots))
+                if posicao == slot_inicial:
+                    parar = True
+        return valor
+
+    def __getitem__(self, chave):
+        return self.get(chave)
+
+    def __len__(self):
+        return self._tamanho
+
+    def __setitem__(self, chave, valor):
+        self.put(chave, valor)
